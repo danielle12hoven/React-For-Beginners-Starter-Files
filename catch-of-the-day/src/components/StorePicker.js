eeab1f817c58a18f525691ff497601303f551f35
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2016-12-28 16:19:35 -0500

    problem solved from video 4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,9 +3,10 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className="store-selector"></form>
-      <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" />
+      <form className='store-selector'>
+
+      <h2>'Please Enter A Store'</h2>
+      <input type='text' required placeholder='Store Name' />
       </form>
     )
   }
