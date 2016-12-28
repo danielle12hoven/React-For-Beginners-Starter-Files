8ff09b97c00d69d62c3889dcdaaed14f9f8f31d0
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2016-12-28 16:04:51 -0500

    video 4 problems with code rendering

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector"></form>
+      <h2>Please Enter A Store</h2>
+      <input type="text" required placeholder="Store Name" />
+      </form>
+    )
+  }
+}
+
+
+export default StorePicker;
