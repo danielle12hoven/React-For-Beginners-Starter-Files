f77b9c8e351cf6fb6f1e9d9cfc6c832261242006
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2016-12-29 12:30:26 -0500

    video 12 complete

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -10,8 +10,10 @@ class StorePicker extends React.Component {
     event.preventDefault();
     console.log('you changed the URL');
     //first grab the text from the box
-    console.log(this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log('Going to ${storeId}');
     //second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo('/store/${storeId}')
   }
 
   render() {
@@ -26,5 +28,8 @@ class StorePicker extends React.Component {
   }
 }
 
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
 
 export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -23,5 +23,6 @@ const Root = () => {
   )
 }
 
+
 render(<Root/>, document.querySelector('#main'));
 
